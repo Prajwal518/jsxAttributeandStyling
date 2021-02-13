0efee613ac17fd79c9cefeb45593d4b37743c04c
Prajwal Gowda S <65961288+Prajwal518@users.noreply.github.com>
2021-02-13 14:19:50 +0530

    jsx styling .we can use javascript properties as attributes in jsx .attributes having more than 1 word needs to use camel casing(className) .we can use js expression as attributes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/id/1003/367/267";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading">My Favourite Foods</h1>
+    <div>
+      <img src={img + "?grayscale"} />
+    </div>
   </div>,
   document.getElementById("root")
 );
